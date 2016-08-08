2570e0bdf3862b3a4d79b329930f2ee53b02721b
Ubuntu <garv@UbuntuServer.0xc0el5qh2iezi3f4s5jde21bg.ix.internal.cloudapp.net>
2016-08-08 10:31:38 +0000

    Created profile page
    Summary: Added profile page. Ref T7
    
    Test Plan: Sample
    
    Reviewers: userReviewer
    
    Reviewed By: userReviewer
    
    Maniphest Tasks: T7
    
    Differential Revision: http://13.76.244.254/D10

---
+++ profile.cs
@@ -0,0 +1,17 @@
+#sample code
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoBikeProj.Startup))]
+namespace demoBikeProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+//Commented and corrected.
+        }
+    }
+}
